5686388ed03fe5d767c1a17ca8ff07daaeeb9e41
Dmitry Ponyatov <dponyatov@gmail.com>
2016-09-26 13:10:26 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
-class Hello {
-		public static void main(String[] args) {
-				System.out.println("Hello");
-		}
+import java.security.MessageDigest;
+
+public class Hello {
+
+    public static void main(String[] args) throws Exception {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] digest = md.digest("abracadabra".getBytes("UTF-8"));
+        for (byte b : digest) {
+            System.out.printf("%02x", b);
+        }
+    }
 }
